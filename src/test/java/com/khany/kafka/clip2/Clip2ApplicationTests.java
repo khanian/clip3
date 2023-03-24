c99fcd1228c0a28c0ae5ee8240199d93bcc33ce8
이강훈 <khanylee@gmail.com>
2023-03-24 14:14:18 +0900

    Initial commit

---
+++ src/test/java/com/khany/kafka/clip2/Clip2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khany.kafka.clip2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clip2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
