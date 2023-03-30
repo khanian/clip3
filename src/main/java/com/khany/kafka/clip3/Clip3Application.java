ad5c1306b242e3ed697d8f71d0fd2b04e2a1cbb3
p163550 <khanylee@gmail.com>
2023-03-30 21:32:09 +0900

    변경

--- src/main/java/com/khany/kafka/clip3/Clip3Application.java
+++ src/main/java/com/khany/kafka/clip3/Clip3Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.nio.charset.StandardCharsets;
+
 @SpringBootApplication
 public class Clip3Application {
 
@@ -19,7 +21,10 @@ public class Clip3Application {
         return args -> {
             clipProducer.async("clip3", "Hello, Clip3-async.");
             clipProducer.sync("clip3", "Hello clip3-sync.");
+            //Thread.sleep(1000L);
             clipProducer.routingSend("clip3", "Hello clip3-routing");
+            clipProducer.routingSendBytes("clip3-bytes", "Hello clip3-bytes".getBytes(StandardCharsets.UTF_8));
+            clipProducer.replyingSend("clip3-request", "Ping Clip3");
         };
     }
 }
